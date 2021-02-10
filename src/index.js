3ebefbd16d72cdf621d42aecbc2ffe798b3be2a6
Rakitzis45 <michael.rakitzis@gmail.com>
2021-02-10 14:10:50 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={(event) => event.target.innerHTML = "I am just sad."} />
   </div>,
   document.getElementById('root')
 );
